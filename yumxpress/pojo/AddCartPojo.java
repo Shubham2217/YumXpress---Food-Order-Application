42e21822d0bcd95c082617285a0c929dd2b6863d
Shubham Yadav <shubhamyadav0229@gmail.com>
2025-05-17 19:11:57 +0530

    Add files via upload

---
+++ yumxpress/pojo/AddCartPojo.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yumxpress.pojo;
+
+/**
+ *
+ * @author Shubham
+ */
+public class AddCartPojo {
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    private String customerId ;
+    private String productId ;
+    private String orderId ;
+    private String companyId ;
+}
